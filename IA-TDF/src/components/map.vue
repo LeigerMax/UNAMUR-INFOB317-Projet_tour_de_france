2c979f791757b7187180edd33d8d57b42d6bdbfe
PastoriLucas <lucas.pastori@student.unamur.be>
2023-03-17 14:50:46 +0100

    Update App.vue, HelloWorld.vue, and 11 more files...

---
+++ IA-TDF/src/components/map.vue
@@ -0,0 +1,21 @@
+<template>
+    <img src="map" alt="map TDF" width="500" height="600" />
+</template>
+  
+<script>
+  import map from '../assets/carte.png'
+  export default {
+    data() {
+      return {
+        
+      };
+    }
+  };
+</script>
+  
+<style scoped>
+  span {
+    color: white;
+    padding-top: 12em;
+  }
+</style>
